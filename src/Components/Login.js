991b65af8ebdbeefb53645b68b97e0448c29639a
jigyasa-ctrl <jigyasaupadhyay60@gmail.com>
2021-09-08 21:15:18 +0530

    signup page added

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,32 +1,32 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
-function Login() {
-    return (
-        <div className="login">
-           <div className="login-side">
-               <h1>Dash.</h1>
-           </div>
-           <div className="login-form">
-               <h1>Sign In</h1>
-               <p className="sign-in">Sign in to your account</p>
-               <span>
-                  <button>Signin with google</button>
-                  <button>Signin with Apple</button>
-               </span>
-               <form className="form">
-                   <label>Email Address</label>
-                   <input type="email" placeholder="enter your email"></input>
-                   <label>Password</label>
-                   <input type="password" placeholder="enter your password"></input>
+function Login({ signup }) {
+  return (
+    <div className="login">
+      <div className="login-side">
+        <h1>Dash.</h1>
+      </div>
+      <div className="login-form">
+        <h1>Sign In</h1>
+        <p className="sign-in">Sign in to your account</p>
+        <span>
+          <button>Signin with google</button>
+          <button>Signin with Apple</button>
+        </span>
+        <form className="form">
+          <label>Email Address</label>
+          <input type="email" placeholder="Enter Your Email"></input>
+          <label>Password</label>
+          <input type="password" placeholder="Enter Your Password"></input>
 
-                   <a>Forgot password?</a>
-                   <button>Sign in</button>
-
-               </form>
-               <p className="register">Don't have an account ? Register Here</p>
-           </div>
-        </div>
-    )
+          <a>Forgot password?</a>
+          <button><Link className="link-class" to="/dashboard">Sign in</Link></button>
+        </form>
+        <p className="register">Don't have an account ? <Link to="/signup"> Register Here</Link></p>
+      </div>
+    </div>
+  );
 }
 
-export default Login
+export default Login;
--- src/Components/Signup.js
+++ src/Components/Signup.js
@@ -1,11 +1,45 @@
-import React from 'react'
+import React from "react";
+import { Link } from 'react-router-dom'
 
-function Signup() {
-    return (
-        <div>
-            <h1>Signup</h1>
-        </div>
-    )
+function Signup({ signup }) {
+  return (
+    <div className="login">
+      <div className="login-side">
+        <h1>Dash.</h1>
+      </div>
+      <div className="login-form signup">
+        <h1>Create an account</h1>
+        <p className="sign-in">Create an account to use dashboard</p>
+        <span>
+          <button>Signin with google</button>
+          <button>Signin with Apple</button>
+        </span>
+        <form className="form">
+          <div className="form-sub">
+            <span className="form">
+              <label>First Name</label>
+              <input type="name" placeholder="First Name"></input>
+            </span>
+            <span className="form">
+              <label>Last Name</label>
+              <input type="name" placeholder="Last Name"></input>
+            </span>
+          </div>
+          <label>Email Address</label>
+          <input type="email" placeholder="Enter Your Email"></input>
+          <label>Password</label>
+          <input type="password" placeholder="Enter Your Password"></input>
+
+          <label>Confirm Password</label>
+          <input type="password" placeholder="Confirm Password"></input>
+
+          <a>Forgot password?</a>
+          <button><Link className="link-class" to="/">Sign up</Link></button>
+        </form>
+        <p className="register">Already have an account ? <Link to="/">Login Here</Link></p>
+      </div>
+    </div>
+  );
 }
 
-export default Signup
+export default Signup;
